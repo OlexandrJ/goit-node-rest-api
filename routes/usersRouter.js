6960cd4c1dd8fd82f3e402db2c94d6e3e9e74307
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2024-03-03 12:10:22 +0200

    fix-fix

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,10 +2,13 @@ import express from 'express';
 import { registerUser, loginUser } from '../controllers/usersControllers.js';
 import validateBody from '../helpers/validateBody.js';
 import { registrationSchema, loginSchema } from "../schemas/userSchemas.js";
+import authMiddleware from '../middleware/authMiddleware.js';
+import { getCurrentUser } from '../controllers/usersControllers.js';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/register', validateBody(registrationSchema), registerUser);
 usersRouter.post('/login', validateBody(loginSchema), loginUser);
+usersRouter.get('/current', authMiddleware, getCurrentUser);
 
 export default usersRouter;
